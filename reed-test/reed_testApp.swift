e3867333f95f58e954f0112adfacd0e6e030c556
jparkerrogers <parker.rogers24@gmail.com>
2025-11-18 10:03:06 -0800

    Initial Commit

---
+++ reed-test/reed_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  reed_testApp.swift
+//  reed-test
+//
+//  Created by J Parker Rogers on 11/18/25.
+//
+
+import SwiftUI
+
+@main
+struct reed_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
